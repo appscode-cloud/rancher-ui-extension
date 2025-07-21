ba8e7253e1bb634cf85c815258a3d01d622f0c28
sourav-roy <1902036.souravroy@gmail.com>
2025-07-21 18:20:17 +0600

    Modal added from rancher dialog
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>

--- pkg/kubedb/pages/Modal.vue
+++ pkg/kubedb/pages/Modal.vue
@@ -1,10 +1,5 @@
-
 <template>
   <div>
-    <button class="btn role-primary" @click="showDialog = true">
-      Open Modal
-    </button>
-
     <Dialog
       v-if="showDialog"
       name="example-modal"
@@ -14,6 +9,7 @@
     >
       <template #default>
         <p>This is a custom modal body. You can put anything here.</p>
+        <p>Rancher UI</p>
       </template>
     </Dialog>
   </div>
@@ -23,24 +19,15 @@
 import { ref } from 'vue';
 import Dialog from '@shell/components/Dialog.vue';
 
-const showDialog = ref(false);
-const lastResult = ref(null);
-
+const showDialog = ref(true);
 
 function handleOkay(callback: (ok: boolean) => void) {
-  callback(true); 
-  showDialog.value = false; 
+  callback(true);
+  showDialog.value = false;
 }
 </script>
 
 <style scoped>
-.dialog-container {
-  padding: 24px;
-  max-width: 500px;
-  margin: 0 auto;
-  text-align: center;
-}
-
 .btn {
   padding: 10px 20px;
   background-color: #007bff;
@@ -52,18 +39,5 @@ function handleOkay(callback: (ok: boolean) => void) {
 
 .btn:hover {
   background-color: #0056b3;
-}
-
-.dialog-content {
-  padding: 16px;
-  font-size: 14px;
-}
-
-.result {
-  margin-top: 24px;
-  padding: 16px;
-  background-color: #f0f0f0;
-  border-radius: 4px;
-  text-align: left;
 }
 </style>
