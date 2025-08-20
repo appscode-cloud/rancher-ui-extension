2666f37c90a792fae93a27c709bb8c4524d6f8ef
SayedTahsin <tahsin@appscode.com>
2025-08-20 16:09:33 +0600

    clear cookies on log out and generates charts
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- pkg/kubedb/index.ts
+++ pkg/kubedb/index.ts
@@ -1,6 +1,20 @@
 import { importTypes } from "@rancher/auto-import";
-import { IPlugin } from "@shell/core/types";
+import {
+  IPlugin,
+  OnLogOut,
+  OnNavToPackage,
+  OnNavAwayFromPackage,
+} from "@shell/core/types";
 import extensionRouting from "./routing/extension-routing";
+import { remove } from "tiny-cookie";
+
+const onEnter: OnNavToPackage = async (store, config) => {};
+const onLeave: OnNavAwayFromPackage = async (store, config) => {};
+const onLogout: OnLogOut = async (store) => {
+  remove("_user_jwt");
+  remove("_user_seed");
+  remove("_nats_url");
+};
 
 // Init the package
 export default function (plugin: IPlugin): void {
@@ -14,4 +28,6 @@ export default function (plugin: IPlugin): void {
   plugin.addProduct(require("./kubedb-config"));
 
   plugin.addRoutes(extensionRouting);
+
+  plugin.addNavHooks(onEnter, onLeave, onLogout);
 }
